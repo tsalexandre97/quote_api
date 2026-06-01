110be91993e5d76c7b24c3c705d3a13e4f53e114
Tiago Alexandre <tiago.alexandre@listenx.com.br>
2026-05-31 22:07:01 -0300

    fix: restore passing test

--- tests/quote.test.js
+++ tests/quote.test.js
@@ -5,6 +5,6 @@ describe("GET /quote", () => {
   it("deve retornar status 200", async () => {
     const response = await request(app).get("/quote");
 
-    expect(response.statusCode).toBe(500);
+    expect(response.statusCode).toBe(200);
   });
 });
